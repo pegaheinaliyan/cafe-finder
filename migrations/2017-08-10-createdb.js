f96051afe3a296c76df954ef2a0dd2d753469b7c
ccadmin <ccadmin@ccadmins-Mac-mini-2.local>
2017-08-11 09:22:37 +0900

    migration

---
+++ migrations/2017-08-10-createdb.js
@@ -0,0 +1,18 @@
+// exports.up = function(knex) {
+//   return knex.schema.createTable('cafeinfo', (table) => {
+
+//     table.increments()
+//       .index();
+
+//     table.float('latitude');
+
+//     table.float('longitude');
+
+//     table.text('name')
+//       .notNullable();
+
+//   });
+// };
+
+// exports.down = function(knex, Promise) {
+// };
